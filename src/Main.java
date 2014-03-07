be3ebacf1dafadb8871999359b5cfe6174d5f843
SevakAvet <Sevak_Avet@mail. ru>
2014-03-08 00:22:52 +0400

    first commit from tablet

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
-public class Main
+
+import java.util.*;public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
+		List<String> l = Arrays.asList("123", "124", "125");
+		Collections.sort(l, new Comparator<String>(){
+			@Override
+			public int compare(String a, String b){
+				return b.compareTo(a);
+			};
+		});
+		
+		System.out.println(l);
 	}
 }
