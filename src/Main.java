f99a6e0ebffaecefac0dc7b9660ecb1107f53bbf
SevakAvet <Sevak_Avet@mail. ru>
2014-03-08 00:26:39 +0400

    delete

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import java.util.*;public class Main
 	public static void main(String[] args)
 	{
 		List<String> l = Arrays.asList("123", "124", "125");
-		// it's work!!!
 		Collections.sort(l, new Comparator<String>(){
 			@Override
 			public int compare(String a, String b){
